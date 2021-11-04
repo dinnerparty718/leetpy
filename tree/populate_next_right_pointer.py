7567d03f56b755ae0cd28f0d3f9e40900599b820
Chen, Ying Ying <Ying-Ying.Chen@marsh.com>
2021-11-04 13:05:58 -0400

    update

---
+++ tree/populate_next_right_pointer.py
@@ -0,0 +1,55 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return None
+
+        leftmost = root
+        curr = leftmost
+
+        while leftmost.left:
+
+            while curr:
+                curr.left.next = curr.right
+                if(curr.next):
+                    curr.right.next = curr.next.left
+                curr = curr.next
+            leftmost = leftmost.left
+            curr = leftmost
+
+        return root
+
+
+solution = Solution()
+root = Node(1)
+
+root.left = Node(2)
+root.right = Node(3)
+
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+root.right.left = Node(6)
+root.right.right = Node(7)
+
+solution.connect(root)
+
+print(root)
