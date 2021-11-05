02e1988a5680f6aea2a342d4d44f13dbaf61a223
Chen, Ying Ying <Ying-Ying.Chen@marsh.com>
2021-11-05 17:36:30 -0400

    wip

---
+++ tree/Codec_dfs.py
@@ -0,0 +1,75 @@
+from typing import List
+from traversal.dfs_recursion import preOrder, postOrder
+from Tree.TreeNode import TreeNode
+
+
+class Codec:
+
+    # preorder
+    def serialize(self, root):
+
+        res = []
+
+        def helper(node: TreeNode):
+            if node:
+                res.append(str(node.val))
+            else:
+                res.append('')
+                return
+
+            helper(node.left)
+            helper(node.right)
+
+        helper(root)
+
+        return ','.join(res)
+
+    def deserialize(self, data):
+
+        if not data:
+            return
+        res: List[str] = data.split(',')
+
+        def helper():
+            if not res:
+                return
+
+            val = res.pop(0)
+            if val == '':
+                return None
+            else:
+                node = TreeNode(int(val))
+
+            node.left = helper()
+            node.right = helper()
+            return node
+
+        return helper()
+
+
+root = TreeNode(1)
+
+left, right = TreeNode(2), TreeNode(3)
+
+root.left = left
+root.right = right
+
+root.right.left, root.right.right = TreeNode(4), TreeNode(5)
+
+
+root.right.left.left, root.right.left.right = TreeNode(6), TreeNode(7)
+
+# root.right.left.left.left = TreeNode(8)
+
+# root.right.left.left.left.left = TreeNode(10)
+
+# root.right.left.right.right = TreeNode(9)
+
+solution = Codec()
+
+str = solution.serialize(root)
+root = solution.deserialize(str)
+
+print(str)
+
+preOrder(root)
