4cfc9c9b297c2c863dd83bdbcaf70b98e94194f4
Chen, Ying Ying <Ying-Ying.Chen@marsh.com>
2021-11-09 15:43:21 -0500

    target sum with DP

--- Queue_Stack/target_sum.py
+++ Queue_Stack/target_sum.py
@@ -1,4 +1,6 @@
+from functools import cache
 from typing import List
+#from functools import cache
 
 # dfs stack or recursive
 
@@ -8,35 +10,55 @@ from typing import List
 class Solution:
 
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        memo = {}
 
-        count = 0
-
-        def helper(nums, i, sum, target):
+        def helper(i, sum):
             # reference count to ther outer scope
             # but not global scope either
-            nonlocal count
+
+            key = (i, sum)
+
+            if key in memo:
+                return memo[key]
 
             # base case
             if i == len(nums):
                 if sum == target:
-                    count += 1
+                    return 1
+                else:
+                    return 0
             else:
-                helper(nums, i + 1, sum + nums[i], target)
-                helper(nums, i + 1, sum - nums[i], target)
 
-        helper(nums, 0, 0, target)
+                to_return = helper(
+                    i + 1, sum - nums[i]) + helper(i + 1, sum + nums[i])
 
-        return count
+                memo[key] = to_return
+                return to_return
+
+        return helper(0, 0)
+
+    def findTargetSumWays2(self, nums: List[int], target: int) -> int:
+
+        @cache
+        def dp(i, sum):
+            if i == len(nums):
+                if target == sum:
+                    return 1
+                else:
+                    return 0
+            else:
+                return dp(i+1, sum + nums[i]) + dp(i+1, sum - nums[i])
+        return dp(0, 0)
 
 
 so = Solution()
 
-nums = [1, 1, 1, 1, 1]
-target = 3
+# nums = [1, 1, 1, 1, 1]
+# target = 3
 
-# nums = [0, 38, 42, 31, 13, 10, 11, 12, 44,
-#         16, 38, 17, 22, 28, 9, 27, 20, 35, 34, 39]
-# target = 2
+nums = [0, 38, 42, 31, 13, 10, 11, 12, 44,
+        16, 38, 17, 22, 28, 9, 27, 20, 35, 34, 39]
+target = 2
 
-res = so.findTargetSumWays(nums, target)
+res = so.findTargetSumWays2(nums, target)
 print(res)
