008f4c1c62101050ea45cb51bafb97fe5c51cbaa
Chen, Ying Ying <Ying-Ying.Chen@marsh.com>
2021-11-29 10:12:42 -0500

    add

--- Dynamic_Programming/subsets.py
+++ Dynamic_Programming/subsets.py
@@ -1,6 +1,43 @@
 from typing import List
 
 
+# time O(n2)
+# backtracking
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        pass
+        self.res = []
+
+        # taken first, then remove
+        def dfs(index: int, nums: List[int], subset: List[int]):
+            if index >= len(nums):
+                # !important added a copy of subset
+                self.res.append(subset[:])
+                return
+            subset.append(nums[index])
+            dfs(index + 1, nums, subset)
+            subset.pop()
+            dfs(index + 1, nums, subset)
+
+        dfs(0, nums, [])
+
+        return self.res
+
+
+# todo
+class Solution2:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = [[]]
+
+        for n in nums:
+            for i in range(len(res)):
+                res.append(res[i]+[n])
+        return res
+
+
+so = Solution2()
+
+nums = [1, 3, 5]
+
+res = so.subsets(nums)
+
+print(res)
--- Dynamic_Programming/template.md
+++ Dynamic_Programming/template.md
@@ -5,3 +5,22 @@ Search 当一个大问题是由多个子问题构成是， 我们可以通过不
 搜索空间(search space)可以用tree的形式展现出来， 便于理解
 时间复杂试取决于这棵树的深度和每个node的children个数
 **Search最重要的就是定义好状态，保证每个子问题都能用一个状态来描述**
+
+如果search space有重复子问题，可以记录这些子问题的答案来保证不会重复计算多次，所以DP也被称为Search + Memoiration
+
+**所有DP都可以写成bottom up DFS 的形式**
+
+
+## Topdown DFS
+
+1. Define STATE of subproblems
+2. Initialize inital state
+3. Call dfs(init_state)
+
+
+dfs(state):
+1. Base case check
+2. For each subproblem x
+   1. a update state = next_state_x
+   2. branch down -> call dfs(next_state_x)
+   3. **Restore State**
