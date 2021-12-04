94495d2ccef59e9846a0d1352e9d056aeb8a2315
Chen, Ying Ying <Ying-Ying.Chen@marsh.com>
2021-12-03 23:06:41 -0500

    dded

---
+++ Arrays_Strings/max_sub_array.py
@@ -0,0 +1,29 @@
+from typing import List
+
+# maxisum subarray
+# Kadane's algorithm
+# https://www.youtube.com/watch?v=86CQq3pKSUw
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+
+        curSum = maxSub = nums[0]
+
+        for num in nums[1:]:
+            # first step compare the current element and current sum
+
+            curSum = max(num, curSum + num)
+            maxSub = max(maxSub, curSum)
+
+        return maxSub
+
+
+so = Solution()
+
+nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+# nums = [1]
+
+res = so.maxSubArray(nums)
+
+print(res)
---
+++ Arrays_Strings/sliding_window.md
@@ -0,0 +1,9 @@
+# slding window technqiue
+
+# l r
+
+[link] https://www.youtube.com/watch?v=MK-NZ4hN7rs
+
+
+
+
