df0800dee8ae2d3261a709c8a4d7feefcd9e0c1a
Chen, Ying Ying <Ying-Ying.Chen@marsh.com>
2021-12-23 23:39:41 -0500

    added

---
+++ _learning/Sorted_Containers/Sorted_Containers.py
@@ -0,0 +1,59 @@
+import collections
+from sortedcontainers import SortedDict, SortedList, SortedSet
+# list append() pop() pop(0)
+# set add() remove() discard()
+# dict d['key']   del d['key']   d.pop(key)
+
+# https://www.youtube.com/watch?v=7z2Ki44Vs4E
+
+# dict
+
+d = SortedDict({2: 2, 9: 9, 1: 1, 3: 3})
+d.get(3)
+
+print('sorted dict')
+
+
+d.setdefault(4, [])
+
+
+d[4].append(4)
+
+
+# delete a key
+del d[4]
+
+# give a default wont raise key error
+a = d.pop(4, [])
+
+
+# delete key
+# del d[4]
+
+
+print(d)
+
+# remove last one
+d.popitem()
+
+print(d)
+print('\nsorted set')
+
+# set
+sorted_set = SortedSet([1, 1, 1, 12, 34])
+sorted_set.add(2)
+
+
+sorted_set.remove(1)  # remove non eixist element will raise error
+sorted_set.discard(3)
+
+print(sorted_set)
+
+
+# list
+
+
+print('\nsorted list')
+
+sorted_list = SortedList([4, 3, 2, 7, 8])
+print(sorted_list)
---
+++ sky_line.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+from sortedcontainers import SortedDict
+
+
+class Solution:
+    def getSkyline(self, buildings: List[List[int]]) -> List[List[int]]:
+        pass
+
+
+# sorted
