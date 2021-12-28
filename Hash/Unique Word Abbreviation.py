f0514f47a79aad59c92986dfac7d9e0f93a85990
Chen, Ying Ying <Ying-Ying.Chen@marsh.com>
2021-12-27 22:39:14 -0500

    added

---
+++ Hash/Unique Word Abbreviation.py
@@ -0,0 +1,27 @@
+from typing import List
+from collections import defaultdict
+
+
+class ValidWordAbbr:
+
+    def __init__(self, dictionary):
+        self.dic = defaultdict(set)
+        for s in dictionary:
+            val = s
+            if len(s) > 2:
+                s = s[0]+str(len(s)-2)+s[-1]
+            self.dic[s].add(val)
+
+    def isUnique(self, word):
+        val = word
+        if len(word) > 2:
+            word = word[0]+str(len(word)-2)+word[-1]
+        # if word abbreviation not in the dictionary, or word itself in the dictionary (word itself may
+        # appear multiple times in the dictionary, so it's better using set instead of list)
+        return len(self.dic[word]) == 0 or (len(self.dic[word]) == 1 and val == list(self.dic[word])[0])
+
+
+dictionary = ["deer", "door", "cake", "card"]
+
+obj = ValidWordAbbr(dictionary)
+print(obj.isUnique('cake'))
