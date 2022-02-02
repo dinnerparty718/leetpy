d071b38325bb6d9ef254d8345a24def5f3852cbc
Yingying Chen <dinnerparty718@gmail.com>
2022-02-01 20:33:06 -0700

    added

--- practise/Word Squares/Word Squares.py
+++ practise/Word Squares/Word Squares.py
@@ -2,13 +2,84 @@ from typing import List
 # https://leetcode.com/problems/word-squares/solution/
 
 
+#  backtrack using trie
 class Solution:
     def wordSquares(self, words: List[str]) -> List[List[str]]:
-        pass
+
+        n = len(words[0])
+        res = []
+
+        contains = '#'
+
+        def buildTrie(words: List[str]):
+            trie = {}
+            #! better to idx instead of word
+            for idx, word in enumerate(words):
+                cur = trie
+
+                for w in word:
+                    if w not in cur:
+                        cur[w] = {}
+                        cur[w][contains] = []
+
+                    cur = cur[w]
+
+                    cur[contains].append(word)
+
+            return trie
+
+        trie = buildTrie(words)
+
+        def get_words_from_trie(prefix: str) -> List[str]:
+            cur = trie
+
+            for char in prefix:
+                if char not in cur:
+                    return []
+                cur = cur[char]
+
+            return cur[contains]
+
+        def getWordsByPrefix(prefix: str) -> List[str]:
+            n = len(prefix)
+            res = []
+
+            for word in words:
+                if word[:n] == prefix:
+                    res.append(word)
+
+            return res
+
+        def backtrack(index: int, curr: List[List[str]]):
+            if index == n:
+                res.append(curr[:])
+                return
+
+            prefix = ''
+
+            for row in range(index):
+                prefix += curr[row][index]
+
+            # candidates = getWordsByPrefix(prefix)
+            candidates = get_words_from_trie(prefix)
+
+            if candidates:
+                for can in candidates:
+                    curr.append(can)
+                    backtrack(index+1, curr)
+                    curr.pop()
+
+        for word in words:
+            curr = [word]
+            backtrack(1, curr)
+
+        return res
 
 
 so = Solution()
-words = ["area", "lead", "wall", "lady", "ball"]
+words = ["ball", "area", "lead", "wall", "lady"]
+
+# words = ["abat", "baba", "atan", "atal"]
 
 res = so.wordSquares(words)
 
---
+++ practise/Word Squares/test.py
@@ -0,0 +1,26 @@
+
+
+from typing import List
+
+
+matrix = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16]
+]
+
+
+def transpose(matrix: List[List[int]]):
+    n = len(matrix)
+
+    for i in range(n):
+        for j in range(i+1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+
+transpose(matrix)
+
+
+for row in matrix:
+    print(row)
