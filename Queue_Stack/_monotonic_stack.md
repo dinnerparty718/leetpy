f26d0c45a30141b886c9d380198cbf1c64071f9c
Yingying Chen <dinnerparty718@gmail.com>
2022-02-06 14:48:16 -0700

    added

---
+++ Queue_Stack/_monotonic_stack.md
@@ -0,0 +1,8 @@
+单调栈
+prev smaller element
+r->l
+1 3 5 7
+
+l->r
+next smaller element
+1 2 5 7 
--- _amazon/test2.py
+++ _amazon/test2.py
@@ -1,15 +1,47 @@
-class Soloution:
-    def __init__(self) -> None:
-        pass
+from typing import List
 
-    def test2(self):
-        pass
+# i j
+
+# sum(  min() * sum())
+
+# https://www.youtube.com/watch?v=TZyBPy7iOAw
+
+
+class Soloution:
+
+    def getPower(self, powers: List[int]):
+
+        n = len(powers)
+
+        cum_sum = [0] * n
+
+        for idx, val in enumerate(powers):
+            if idx == 0:
+                cum_sum[idx] = val
+            else:
+                cum_sum[idx] = cum_sum[idx-1] + val
+
+        for i in range(n):
+            for j in range(i, n):
+                sub = powers[i:j+1]
+
+                sub_sum = cum_sum[j] - (cum_sum[i-1] if i != 0 else i)
+
+                print(i, j, ' SUM ', sub, min(sub))
+
+        res = 0
+        return res % (10**9 + 7)
 
 
 so = Soloution()
 
+powers = [2, 3, 2, 1]
 
-res = so.test2()
+res = so.getPower(powers)
 
 
 print(res)
+
+
+# [1 2,3,4,5,6]
+# [6,5,4,3,2,1]
