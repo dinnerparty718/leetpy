2d6fa9505798a1f000304afc5fdade02bf85353e
Yingying Chen <dinnerparty718@gmail.com>
2022-02-14 10:44:51 -0700

    added

--- _topic/DP/2. Zero One Knapsack/Partition Equal Subset Sum.py
+++ _topic/DP/2. Zero One Knapsack/Partition Equal Subset Sum.py
@@ -105,20 +105,47 @@ class Solution:
         return target in dp
 
 
-# leetcode 2D DP
+# 2D DP not from leetcode
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         total = sum(nums)
+        n = len(nums)
 
         if total % 2 == 1:
             return False
 
+        target = total // 2
+
         # dp
 
+        dp = [[None] * (target + 1) for _ in range(n)]
 
-nums = [1, 5, 11, 5]
+        #! populate 1st col, target 0 True
+
+        for i in range(n):
+            dp[i][0] = True
+
+        #! populate 1st row
+
+        for t in range(1, target + 1):
+            dp[0][t] = nums[0] == t
+
+        for i in range(1, n):
+            for j in range(1, target+1):
+                curr = nums[i]
+
+                if j < curr:
+                    dp[i][j] = dp[i-1][j]
+                else:
+                    dp[i][j] = dp[i-1][j] or dp[i-1][j - curr]
+
+        return dp[-1][-1]
+
+
+# nums = [1, 5, 11, 5]
 # nums = [1, 2, 3, 5]
-
+nums = [2, 2, 1, 1]
+nums = [2, 2, 3, 5]
 
 so = Solution()
 res = so.canPartition(nums)
