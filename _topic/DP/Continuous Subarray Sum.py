9cb179d7f9b735042be81ccc9eb65f6635be71b3
Yingying Chen <dinnerparty718@gmail.com>
2022-02-15 08:54:32 -0700

    added

---
+++ _topic/DP/Continuous Subarray Sum.py
@@ -0,0 +1,6 @@
+from typing import List
+
+
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        pass
--- _topic/DP/Maximum Score from Performing Multiplication Operations.py
+++ _topic/DP/Maximum Score from Performing Multiplication Operations.py
@@ -8,6 +8,11 @@ from functools import lru_cache
 # top down
 # time limit or memery limit
 
+#! exceed limit
+
+# todo
+# bottom up
+
 
 class Solution:
     def maximumScore(self, nums: List[int], multipliers: List[int]) -> int:
@@ -15,20 +20,24 @@ class Solution:
         n = len(nums)
         m = len(multipliers)
 
+        # state i, left, right
+
         # dp return max score after i interation
         # dp(0,0)
+
         @lru_cache(2000)
-        def dp(i: int, left: int) -> int:
+        def dfs(i: int, left: int) -> int:
             if i == m:
                 return 0
-
             mult = multipliers[i]
-
             right = n - 1 - (i - left)
+            return max(dfs(i+1, left+1) + mult * nums[left], dfs(i+1, left) + mult * nums[right])
 
-            return max(dp(i+1, left+1) + mult * nums[left], dp(i+1, left) + mult * nums[right])
+        res = dfs(0, 0)
 
-        return dp(0, 0)
+        #! clear the cache before returning
+        dfs.cache_clear()
+        return res
 
 
 # bottom up
@@ -39,7 +48,14 @@ class Solution:
         n, m = len(nums), len(multipliers)
         dp = [[0] * (m+1) for _ in range(m+1)]
 
-        print(dp)
+        for i in range(m-1, -1, -1):
+            for left in range(i, -1, -1):
+                mult = multipliers[i]
+                right = n - 1 - (i - left)
+                # use left                             or   use right
+                dp[i][left] = max(mult * nums[left] + dp[i+1][left+1],    dp[i+1][left] + mult * nums[right])
+
+        return dp[0][0]
 
 
 so = Solution()
@@ -58,7 +74,7 @@ print(res)
 
 #! range(start, stop, step)
 
-l = ['a', 'b', 'c', 'd', 'e']
+# l = ['a', 'b', 'c', 'd', 'e']
 
-for i in range(len(l)-1, -1, -1):
-    print(l[i])
+# for i in range(len(l)-1, -1, -1):
+#     print(l[i])
