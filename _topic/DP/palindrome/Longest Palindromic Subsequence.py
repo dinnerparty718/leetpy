78de3f2c0ee57ec86f01ff82b52efe860ac7c078
Yingying Chen <dinnerparty718@gmail.com>
2022-02-16 14:02:39 -0700

    added

--- _topic/DP/palindrome/Longest Palindromic Subsequence.py
+++ _topic/DP/palindrome/Longest Palindromic Subsequence.py
@@ -16,12 +16,29 @@ Input: s = "cbbd"
 Output: 2
 Explanation: One possible longest palindromic subsequence is "bb".
 
+
+
 '''
+
+#! todo
+
+# https://leetcode.com/problems/longest-palindromic-subsequence/discuss/1096936/PYTHON-DP-FOR-DUMMIES
 
 
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
-        pass
+        memo = {}
+
+        def dfs(s):
+            if s not in memo:
+                maxL = 0
+                for c in set(s):
+                    i, j = s.find(c), s.rfind(c)
+                    maxL = max(maxL, 1 if i == j else 2 + dfs(s[i+1:j]))
+                memo[s] = maxL
+            return memo[s]
+
+        return dfs(s)
 
 
 so = Solution()
