70914e7e262e335aa2f777be04970ee5e02ea65f
Yingying Chen <dinnerparty718@gmail.com>
2022-02-16 12:11:17 -0700

    added

---
+++ _topic/DP/palindrome/Longest Palindromic Subsequence.py
@@ -0,0 +1,32 @@
+'''
+516. Longest Palindromic Subsequence
+
+Given a string s, find the longest palindromic subsequence's length in s.
+
+A subsequence is a sequence that can be derived from another sequence by deleting some or no elements without changing the order of the remaining elements.
+
+
+
+Input: s = "bbbab"
+Output: 4
+Explanation: One possible longest palindromic subsequence is "bbbb".
+
+
+Input: s = "cbbd"
+Output: 2
+Explanation: One possible longest palindromic subsequence is "bb".
+
+'''
+
+
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+        pass
+
+
+so = Solution()
+
+s = "bbbab"
+res = so.longestPalindromeSubseq(s)
+
+print(res)
--- _topic/DP/palindrome/Palindromic Substrings.py
+++ _topic/DP/palindrome/Palindromic Substrings.py
@@ -21,4 +21,33 @@ Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
 
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        pass
+        n = len(s)
+
+        count = 0
+
+        for i in range(n):
+            # odd
+            l, r = i, i
+            while 0 <= l and r < n and s[l] == s[r]:
+                count += 1
+
+                l -= 1
+                r += 1
+
+            l, r = i, i+1
+            while 0 <= l and r < n and s[l] == s[r]:
+                count += 1
+
+                l -= 1
+                r += 1
+
+        return count
+
+
+so = Solution()
+
+s = "aaa"
+
+res = so.countSubstrings(s)
+
+print(res)
