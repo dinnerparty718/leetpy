aa354475f35d07ab9d8361954a448f8e73dc9337
Yingying Chen <dinnerparty718@gmail.com>
2022-02-20 11:48:09 -0700

    added

---
+++ _topic/subarray substrings/Stock Market Prediction.py
@@ -0,0 +1,23 @@
+
+'''
+
+https://leetcode.com/discuss/interview-question/485097/braze-oa-2020-stock-market-prediction
+
+predict stock market price 
+
+10 days
+i-th day 1 index
+
+
+
+O(n)
+next_smaller
+prev_smaller
+
+dist1 = i - prev_smaller
+dist2 = next_smaller - i
+
+'''
+
+stocData = [5, 6, 8, 4, 9, 10, 8, 3, 6, 4]
+qeury = [6, 3, 8]
