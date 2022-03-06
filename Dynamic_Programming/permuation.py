f50b4fce6076da3d21bb8f32d1a627780d7b069c
Yingying Chen <dinnerparty718@gmail.com>
2022-03-06 15:34:30 -0700

    permute

--- Dynamic_Programming/permuation.py
+++ Dynamic_Programming/permuation.py
@@ -1,29 +1,71 @@
 from typing import List
 
+'''
+
+param(curr_idx, arr)
+
+keep swap curr_idx and looping i
+backtrack
+when curr_idx == len(arr), copy arr to res
+swap back
+
+total permulation n! = n(n-1)(n-2)(n-3)*1 
+
+n = 3 , permutations = 3*2*1 = 6
+
+'''
+
 
 class Solution:
-    def permute(self, nums: List[int]) -> List[List[int]]:
-
+    def permute_no_duplicate(self, nums: List[int]) -> List[List[int]]:
         res = []
+        n = len(nums)
 
-        def dfs(nums: List[int], index: int, res: List[List[int]]):
+        def helper(curr_idx: int, curr: List[int]):
 
-            # add a copy of current state to the result set
+            if curr_idx == n:
+                res.append(curr[:])
+                return
 
-            if index >= len(nums):
+            for i in range(curr_idx, n):
+                curr[curr_idx], curr[i] = curr[i], curr[curr_idx]
+                helper(curr_idx+1, curr)
+                curr[curr_idx], curr[i] = curr[i], curr[curr_idx]
+
+        helper(0, nums)
+
+        return res
+
+
+so = Solution()
+
+nums = [1, 2, 3]
+res = so.permute_no_duplicate(nums)
+
+# print(res)
+
+
+class Solution:
+    def permute_with_dup(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        n = len(nums)
+
+        def helper(index: int, nums: List[int]):
+            if index == n:
+
                 res.append(nums[:])
                 return
 
-            num_set = set()
+            number_set = set()
 
-            for i in range(index, len(nums)):
-                nums[i],  nums[index] = nums[index], nums[i]
-                if nums[index] not in num_set:
-                    dfs(nums, index+1, res)
-                    num_set.add(nums[index])
-                nums[i],  nums[index] = nums[index], nums[i]
+            for i in range(index, n):
+                nums[i], nums[index] = nums[index], nums[i]
+                if nums[index] not in number_set:
+                    helper(index+1, nums)
+                    number_set.add(nums[index])
+                nums[i], nums[index] = nums[index], nums[i]
 
-        dfs(nums, 0, res)
+        helper(0, nums)
 
         return res
 
@@ -31,8 +73,6 @@ class Solution:
 so = Solution()
 
 nums = [1, 2, 2]
-res = so.permute(nums)
+res = so.permute_with_dup(nums)
 
-
-for i in res:
-    print(i)
+print(res)
