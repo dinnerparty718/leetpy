9bf209a2a5bd777a77f454656aacbb24a453b9d6
Yingying Chen <dinnerparty718@gmail.com>
2022-03-09 20:44:29 -0700

    added

--- _google/medians two sorted arrays.py
+++ _google/medians two sorted arrays.py
@@ -1,113 +1,72 @@
+'''
+
+Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+The overall run time complexity should be O(log (m+n)).
+
+#! log hints binary search
+#! binary search on the smaller array 
+
+without merging them
+
+
+always round down   total_len // 2
+
+Total 13
+
+6 + 7
+
+
+B 1 2 3, 4 5 6 7 8 
+
+A 1 2 3, 4 5 
+  L        R
+  
+
+when even
+    min(leftA, leftB) , max(rightA, rightB)  
+when odd
+    max(rightA, rightB)
+    
+    
+log(min(n,m))    
+'''
+
 
 from typing import List
 
-'''
 
-median
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        A, B = nums1, nums2
 
-odd
-n // 2
+        total = len(nums1) + len(nums2)
+        half = total // 2
 
+        if len(B) < len(A):
+            A, B = B, A
 
-even
+        l, r = 0, len(A)-1
 
-(n//2 -1  +  n//2) 
+        while True:
+            i = (l + r) // 2  # A
+            j = half - i - 2  # B index
 
-#! same size n
+            Aleft = A[i] if i >= 0 else float('-inf')
+            Aright = A[i+1] if (i + 1) < len(A) else float('inf')
 
-1. Brute Force: merging using extra space
-2. Two-pointers : couting while merging
-3. divide and conquer -> binary search
+            Bleft = B[j] if j >= 0 else float('-inf')
+            Bright = B[j+1] if (j + 1) < len(B) else float('inf')
 
-
-
-
-#! different size m, n
-
-'''
-
-
-#! binary search recursive
-def getMedian(A: List[int], B: List[int], n: int):
-
-    #! base case
-    if n == 0:
-        return 0
-
-    if n == 1:
-        return (A[0] + B[0]) / 2
-
-    middle1 = getArrayMedian(A, n)
-    middle2 = getArrayMedian(B, n)
-
-    if middle1 == middle2:
-        return middle1
-
-    if middle1 < middle2:
-        return getMedian(A + )
-
-    def getArrayMedian(C: List[int], n):
-        if n % 2 == 0:
-            return (C[n//2 - 1] + C[n//2])/2
-        else:
-            return C[n//2]
-
-
-#! two pointer
-#! 2n even length
-#! n-1 n index
-# Time O(n)
-# space O(1)
-def getMedian(A: List[int], B: List[int], n: int):
-    i = 0
-    j = 0
-
-    middle1 = 0
-    middle2 = 0
-    count = 0
-
-    while count <= n:
-
-        if A[i] <= B[j]:
-            middle1 = middle2  # store the previous median
-            middle2 = A[i]
-            i += 1
-        else:
-            middle1 = middle2
-            middle2 = B[j]
-            j += 1
-
-        # edge case
-        # 1 2 3
-        # 5 6 7
-        if i == n:
-            middle1 = middle2
-            middle2 = B[0]
-            break
-        elif j == n:
-            middle1 = middle2
-            middle2 = A[0]
-            break
-
-        count += 1
-
-    return (middle1 + middle2)/2
-
-
-arr1 = [1, 2, 3]
-arr2 = [4, 5, 6]
-
-
-print(getMedian(arr1, arr2, 3))
-
-
-def getMedianFromSortedArray(nums: List[int]) -> float:
-    n = len(nums)
-
-    if n % 2 == 0:
-        return (nums[n//2 - 1] + nums[n//2]) / 2
-    else:
-        return nums[n // 2]
-
-
-nums = [1, 2, 3, 4, 5]
+            # partition is corrent
+            if Aleft <= Bright and Bleft <= Aright:
+                # odd
+                if total % 2:
+                    return min(Aright, Bright)
+                else:
+                    # even
+                    return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
+            elif Aleft > Bright:
+                r = i - 1
+            else:
+                l = i + 1
