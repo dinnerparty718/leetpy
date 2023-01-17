e27dfbd7cfcec143a9ccf17573127184e7973010
Yingying Chen <dinnerparty718@gmail.com>
2023-01-17 06:02:19 -0700

    add

---
+++ string/Space Replacement.py
@@ -0,0 +1,21 @@
+'''
+212 · Space Replacement
+
+1. use new space
+
+
+2. in place
+
+'''
+
+
+class Solution:
+    """
+    @param: string: An array of Char
+    @param: length: The true length of the string
+    @return: The true length of new string
+    """
+
+    def replaceBlank(self, string, length):
+        # write your code here
+        pass
